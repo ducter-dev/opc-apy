2f000c893abc981a9e0fdad32a31bd8411ac06b6
ducter-dev <michel.morales@ducter.com.mx>
2021-12-23 17:12:38 -0600

    :sparkles: Agregando eventos a api

--- project/main.py
+++ project/main.py
@@ -1,14 +1,27 @@
 from fastapi import FastAPI
 
+
 app = FastAPI(
     title='SCADA-IRGE',
     description='Api para servir OPC',
     version='1.0'
 )
 
+
+@app.on_event('startup')
+def startup():
+    print('El servidor va a comenzar.')
+
+
+@app.on_event('shutdown')
+def shutdown():
+    print('El servidor se encuentra finalizado.')
+
+
 @app.get('/')
 async def index():
     return 'Hola mundo desde FastApi'
+
 
 @app.get('/about')
 async def about():
