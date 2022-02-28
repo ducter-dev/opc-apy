fe6bfad0ab07c32cf258378dce3319a05ca85ed9
Michel <59454429+ducter-dev@users.noreply.github.com>
2022-02-28 15:23:11 -0600

    :sparkles: Anexando rutas de acciones (#89)
    * :sparkles: Agregando las acciones de llenaderas.
    
    * :sparkles: Anexando rutas de acciones

--- project/routers/llenaderas.py
+++ project/routers/llenaderas.py
@@ -2,8 +2,10 @@ from fastapi import APIRouter
 from fastapi.responses import JSONResponse
 from typing import List
 
+from project.opc import OpcServices
+
 from ..database import Llenadera
-from ..schemas import LlenaderaRequestModel, LlenaderaResponseModel, EstadoLlenaderaRequesteModel
+from ..schemas import LlenaderaRequestModel, LlenaderaResponseModel, EstadoLlenaderaRequesteModel, NumeroLlenaderaRequesteModel
 
 from ..middlewares import VerifyTokenRoute
 
@@ -58,7 +60,7 @@ async def delete_llenadera(llenadera_id: int):
 
 # ------------ Estado Llenaderas ------------
 @router.post('/estado')
-async def post_changeBarreraSalida(request: EstadoLlenaderaRequesteModel):
+async def post_changeEstado(request: EstadoLlenaderaRequesteModel):
     # 1 = Detener lista de despacho
     # 0 = Liberar lista de despacho
     try:
@@ -75,7 +77,7 @@ async def post_changeBarreraSalida(request: EstadoLlenaderaRequesteModel):
     )
 
 @router.get('/estado')
-async def get_getBarreraSalida():
+async def get_getEstado():
     try:
         #barrera = OpcServices.readDataPLC('GE_ETHERNET.PLC_SCA_TULA.Applications.Radiofrecuencia.EntryExit.RFVER_EDOLISTA')
         barrera = 1
@@ -89,3 +91,98 @@ async def get_getBarreraSalida():
         status_code=501,
         content={"message": e}
     )
+
+# ------------ Acciones de  Llenaderas ------------
+# -> aceptar asignacion
+@router.post('/asignacion/aceptar')
+async def post_aceptarAsignacion(request: EstadoLlenaderaRequesteModel):
+    # 1 = Aceptar asignacion cgRFVER_ACEPTAASIGNA
+
+    try:
+        # OpcServices.writeOPC('GE_ETHERNET.PLC_SCA_TULA.Applications.Radiofrecuencia.EntryExit.RFVER_ACEPTAASIGNA', request.estado)
+        
+        return JSONResponse(
+            status_code=201,
+            content={"message": 'La llenadera ha aceptado la asignacion.'}
+        )
+    except Exception as e:
+        return JSONResponse(
+        status_code=501,
+        content={"message": str(e)}
+    )
+
+# -> cancelar asignacion
+@router.post('/asignacion/cancelar')
+async def post_cancelarAsignacion(request: EstadoLlenaderaRequesteModel):
+    # 1 = Cancelar asignacion RFVER_ELIMINAASIGNA
+
+    try:
+        # OpcServices.writeOPC('GE_ETHERNET.PLC_SCA_TULA.Applications.Radiofrecuencia.EntryExit.RFVER_ELIMINAASIGNA', request.estado)
+        
+        return JSONResponse(
+            status_code=201,
+            content={"message": 'Se ha cancelado la asignacion de la llenadera.'}
+        )
+    except Exception as e:
+        return JSONResponse(
+            status_code=501,
+            content={"message": str(e)}
+        )
+
+# -> reasignar asignacion
+@router.post('/asignacion/reasignar')
+async def post_reasignarAsignacion(request: NumeroLlenaderaRequesteModel):
+    # 1 = Obtener La llenadera disponible del request
+    llenadera = request.llenadera
+    # 2 Escribir el valor en la llenadera que se elija
+
+    try:
+        result = reasignar(llenadera)
+        if result == 0 :
+            return JSONResponse(
+                status_code=402,
+                content={"message": 'Llenadera no existente.'}
+            )
+        #OpcServices.writeOPC(result, 1)
+        return JSONResponse(
+            status_code=201,
+            content={"message": f'Se ha reasignado la llenadera {llenadera}.'}
+        )
+    except Exception as e:
+        return JSONResponse(
+            status_code=501,
+            content={"message": str(e)}
+        )
+
+def reasignar(llenadera):
+    tabla_llenaderas = {
+        5: 'GE_ETHERNET.PLC_SCA_TULA.Asignacion.ASIGN_LLEN5',
+        6: 'GE_ETHERNET.PLC_SCA_TULA.Asignacion.ASIGN_LLEN6',
+        7: 'GE_ETHERNET.PLC_SCA_TULA.Asignacion.ASIGN_LLEN7',
+        8: 'GE_ETHERNET.PLC_SCA_TULA.Asignacion.ASIGN_LLEN8',
+        9: 'GE_ETHERNET.PLC_SCA_TULA.Asignacion.ASIGN_LLEN9',
+        10: 'GE_ETHERNET.PLC_SCA_TULA.Asignacion.ASIGN_LLEN10',
+        11: 'GE_ETHERNET.PLC_SCA_TULA.Asignacion.ASIGN_LLEN11',
+        12: 'GE_ETHERNET.PLC_SCA_TULA.Asignacion.ASIGN_LLEN12',
+        13: 'GE_ETHERNET.PLC_SCA_TULA.Asignacion.ASIGN_LLEN13',
+        14: 'GE_ETHERNET.PLC_SCA_TULA.Asignacion.ASIGN_LLEN14'
+    }
+    return tabla_llenaderas.get(llenadera, 0 )
+
+# -> siguiente asignacion
+@router.post('/asignacion/siguiente')
+async def post_siguienteAsignacion(request: EstadoLlenaderaRequesteModel):
+    # 1 = Siguiente asignacion RFVER_SIGLLENADERA
+
+    try:
+        # OpcServices.writeOPC('GE_ETHERNET.PLC_SCA_TULA.Applications.Radiofrecuencia.EntryExit.RFVER_SIGLLENADERA', request.estado)
+        
+        return JSONResponse(
+            status_code=201,
+            content={"message": 'Ha seleccionado otra llenadera para asignar.'}
+        )
+    except Exception as e:
+        return JSONResponse(
+            status_code=501,
+            content={"message": str(e)}
+        )
