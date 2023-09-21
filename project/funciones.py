3efa08e0488c63a327541d8b93d3a82cf2f99b25
Michel Morales <michel.morales@ducter.com.mx>
2023-09-21 16:52:00 -0600

    :sparkles: Agregada ruta para lectura de reloj opc

--- project/funciones.py
+++ project/funciones.py
@@ -5,7 +5,7 @@ from .logs import LogsServices
 
 def obtenerFecha05Reporte():
     now = datetime.now()
-    fecha_base = datetime(now.year, now.month, now.day, 5, 0, 0)
+    fecha_base = datetime(now.year, now.month, now.day, 5, 30, 0)
     fecha05 = (now - timedelta(days=1)).strftime("%Y-%m-%d") if fecha_base > now else now.strftime("%Y-%m-%d")
     return fecha05
 
